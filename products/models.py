fbd53933c472faa959a82027bab537c5026e6e7c
test_python <khazaei.amir110@gmail.com>
2022-08-24 11:12:43 +0430

    Create Model in Database

--- products/models.py
+++ products/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+"""
+for  Image(model.ImageField) => install Package Pillow
+"""
+
+
 class Category(models.Model):
     """
      verbose_name : چیزی که نمایش داده می شود /// title : چیزی که در دیتابیس ذخیره میشود
@@ -36,7 +41,7 @@ class Product(models.Model):
     description = models.TextField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories/')
     is_enable = models.BooleanField(_('is enable'), default=True)
-    categories = models.ManyToManyField(_('Category'), verbose_name=_('categories'), blank=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
 
