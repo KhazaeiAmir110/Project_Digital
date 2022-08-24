f3439b32c7083ffc7e7d5eb7ef15322cd5bd035b
test_python <khazaei.amir110@gmail.com>
2022-08-24 10:45:20 +0430

    Tree model add modelsProject

--- products/models.py
+++ products/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+     verbose_name : چیزی که نمایش داده می شود /// title : چیزی که در دیتابیس ذخیره میشود
+     _("name") : translation
+     avatar  : uploud Image
+     """
+    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(_('Name'), max_length=60)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    """
+     name table (default) : name(app) + name(model)
+    but : class Meta == name table
+    verbose_name : admin
+    """
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+
+class Product(models.Model):
+    """
+    categories = models.ManyToManyField : One Product => Many Category
+    """
+    title = models.CharField(_('Name'), max_length=60)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    categories = models.ManyToManyField(_('Category'), verbose_name=_('categories'), blank=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
+
+class File(models.Model):
+    """
+    /%Y/%m/%d/ : year / munth / day
+    """
+    product = models.ForeignKey('Product', verbose_name=_('product'), on_delete=models.CASCADE)
+    title = models.CharField(_('Name'), max_length=60)
+    file = models.FileField(_('file'), upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    class Meta:
+        db_table = 'file'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
